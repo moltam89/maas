643006b451003df1ac315a25f9100c8d22a26fcb
tamas.molnar <tamas.molnar@liferay.com>
2022-08-25 20:27:39 +0200

    Override signaturesRequired before simulating the transaction, so we don't have to wait for all the signatures.

--- packages/react-app/src/components/TenderlySimulation.jsx
+++ packages/react-app/src/components/TenderlySimulation.jsx
@@ -36,9 +36,71 @@ export default function TenderlySimulation({ params, address, multiSigWallet}) {
           return;
         }
 
+        // Tenderly magic to override signaturesRequired so we can simulate transactions without all the signatures
+        // https://tenderlydev.notion.site/Sim-API-With-State-Overrides-83d80213689b43de8f3d45e121689b42#0a87b56e05af47ffb2304777f9229843
+        // https://tenderlydev.notion.site/Sim-API-With-State-Overrides-83d80213689b43de8f3d45e121689b42#e00e8c2a9183421a9495fb78742d9c58
+
+        // 1: prepare state overrides. This is where you specify all the contracts you need to override for the simulation.
+        const stateOverridesSpecification = {
+          networkID: `${params.chainId}`, // a STRING: network ID as "3"
+          /* stateOverrides is a specification of assignments: Map<ContractAddress, AssignmentsSpecification>
+              - The key is the contract's address (so you can override state in multiple contracts)
+              - AssignmentsSpecification: The value is an object specifying overrides of state variables' values.
+                To achieve kvStore[1] = 99 add "kvStore[1]": "99" to as an entry of the value map.
+                Left hand side is the key in this JSON ("kvStore[1]") and right hand side of the assignment is the value ("99").
+          */
+          stateOverrides: {
+              [multiSigWallet.address]: {
+                  value: {
+                      // overrides of contract state override (fields come from contract's state vars)
+                      "signaturesRequired": "1",
+                  }
+              }
+          }
+        }
+
+        console.log("encodedStateOverridesstateOverridesSpecification", stateOverridesSpecification);
+
+
+        // 2: Encode state overrides (intermediary step)
+        const ENCODE_STATE_API = `https://api.tenderly.co/api/v1/account/${TENDERLY_USER}/project/${TENDERLY_PROJECT}/contracts/encode-states`;
+        const encodedSatateResponse = await axios.post(ENCODE_STATE_API, stateOverridesSpecification, OPTS);
+        const encodedStateOverrides = encodedSatateResponse.data;
+        console.log("encodedStateOverrides", encodedStateOverrides);
+        console.log("encodedStateOverrides.stateOverrides", encodedStateOverrides.stateOverrides);
+        console.log("encodedStateOverridesmultiSigWallet.address", multiSigWallet.address);
+        console.log("encodedStateOverrides.stateOverrides[multiSigWallet.address]", encodedStateOverrides.stateOverrides[multiSigWallet.address.toLowerCase()]);
+        console.log("encodedStateOverrides.stateOverrides[0x51427a9EC71855bD4a0882ea0A7a2A889B91F80E]", encodedStateOverrides.stateOverrides["0x51427a9EC71855bD4a0882ea0A7a2A889B91F80E"]);
+
+        console.log("multiSigWallet", multiSigWallet);
+
+        // 3: Prepare transaction
+        const unsignedTransactionToSimulate = await multiSigWallet.populateTransaction.signaturesRequired();
+        console.log("unsignedTransactionToSimulate", unsignedTransactionToSimulate);
+
         const value = params.amount ? ethers.utils.parseEther("" + parseFloat(params.amount).toFixed(12)) : "0x00";
         const txData = (params.data && params.data != "0x") ? params.data : "0x";
         let data = multiSigWallet.interface.encodeFunctionData("executeTransaction", [params.to, value, txData, params.signatures]);
+
+        // 4: Create a transaction and pass encodedStateOverrides under state_objects
+        const transactionWithOverrides = {
+            ...unsignedTransactionToSimulate, // 
+            input: data, // input is necessary
+            network_id: `${params.chainId}`, //network ID: a string
+            "from": address, // any address
+            to: multiSigWallet.address,
+
+            /* 
+                This is again a mapping; Map<ContractAddress, {storage: encodedStorageOverrides }> 
+                populate storage with the value in encodedStateOverrides which corresponds  
+            */
+            state_objects: {
+                [multiSigWallet.address]: {
+                    storage: encodedStateOverrides.stateOverrides[multiSigWallet.address.toLowerCase()].value
+                }
+            },
+            save: true // saves to dashboard
+        }
 
         const body = {
           // standard TX fields
@@ -55,14 +117,17 @@ export default function TenderlySimulation({ params, address, multiSigWallet}) {
           "save": true,
           //"simulation_type": "quick"
         }
-      
-        const resp = await axios.post(SIMULATE_URL, body, OPTS);
 
-        if (resp.data.simulation.status === false) {
+        const simResponse = await axios.post(SIMULATE_URL, transactionWithOverrides, OPTS);
+        console.log("Returned value: ", simResponse.data.transaction.transaction_info.call_trace.output);
+      
+        //const resp = await axios.post(SIMULATE_URL, body, OPTS);
+
+        if (simResponse.data.simulation.status === false) {
           setSimulationFailed(true);
         }
 
-        setSimulationId(resp.data.simulation.id);
+        setSimulationId(simResponse.data.simulation.id);
         setSimulated(true);
       }
       catch(error) {
